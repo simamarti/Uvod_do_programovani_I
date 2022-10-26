2ad95b79858eae8c7546181c49a52fb5cf6d1642
Martin Šíma <sima.pacov@seznam.cz>
2022-10-26 16:46:52 +0200

    Pokus_4

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,4 +1,4 @@
-from turtle import down, forward, right, left, exitonclick, goto, speed, up, window_height, window_width
+from turtle import circle, down, forward, right, left, exitonclick, goto, speed, up, window_height, window_width
 
 def draw_Square():      # Začíná nahoře vlevo
     
@@ -32,37 +32,66 @@ def draw(row, column):
         forward(50)
         left(90)
 
+    goto(0, 0)
+
 def Input():
 
-    row = 0
-    column = 0
+    row = None
+    column = None
 
     try:
         
         row = int(input("KOlik políček by měla mít hrací plocha na výšku: "))
+
+        if row <= 0:
+
+            print("Číslo musí být kladné.")
+            return (None, None)
+
         column = int(input("KOlik políček by měla mít hrací plocha na šířku: "))
+
+        if column <= 0:
+
+            print("Číslo musí být kladné.")
+            return (None, None)
 
     except ValueError:
 
         print("Měl jste vložit číslo.")
+        return (None, None)
 
     except OverflowError:
 
         print("Číslo je příliš velké.")
-
-    if row <= 0 or column <= 0:
-
-        print("Číslo musí být kladné.")
-        row = 0
-        column = 0
+        return (None, None)
 
     return (row, column)
 
 def Centers(row, column):
 
-    pass
+    centers = [[None]*row]*column
+
+    coord_x = -window_width()/2 + 25
+    coord_y = window_height()/2 - 25
+
+    for i in range(row):
+
+        for j in range(column):
+
+            centers[i][j] = (coord_x, coord_y)
+            coord_x += 50
+            
+        coord_y -= 50
+        coord_x = -window_width()/2 + 25
+
 row, column = Input()
 
-draw(row, row)
+if row is not None:
+    
+    draw(row, column)
+    centers = Centers(row, column)
+
+
+
 
 exitonclick()
