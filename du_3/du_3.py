d73e1f9fd5ff637714d653a70cfec335d92e7c69
Martin Šíma <sima.pacov@seznam.cz>
2022-12-23 14:45:44 +0100

    Bonus medián, geojson s přiřazenými kontejnery

--- du_3/du_3.py
+++ du_3/du_3.py
@@ -17,27 +17,35 @@ def change_coord(adresses : dist) -> None:  # Převod souřadnic z WGS84 do jtsk
     for item in adresses['features']:
         item['geometry']['coordinates'] = wgstojtsk.transform(*item['geometry']['coordinates'])
 
-def dist_calc(bins : list, coord_adr : list) -> int:
+def dist_calc(bins : list, coord_adr : list) -> tuple:
     min_dist = None
+    id_number = None
     for can in bins:
         coord_bin = can['geometry']['coordinates']
         distance = dist(coord_adr, coord_bin)
         if min_dist == None:
             min_dist = distance
+            id_number = can['properties']['ID']
         elif min_dist > distance:
             min_dist = distance
-    return min_dist
+            id_number = can['properties']['ID']
+    return min_dist, id_number
 
 def process(adresses : dist, bins : dist) -> tuple:
     average = 0
     min_distance = 0
+    dist_array = []
     adress = None
     counter = 0
     max_dist = 0
+    median = 0
+    id_number = 0
     for item in adresses['features']:
         coord_adr = item['geometry']['coordinates']
         counter += 1
-        min_distance = dist_calc(bins['features'], coord_adr)
+        min_distance, id_number = dist_calc(bins['features'], coord_adr)
+        item['kontejner'] = id_number
+        dist_array.append(min_distance)
         average += min_distance
         if min_distance > 10000:
             print(">> minimální vzdálenost k některému kontejneru přesáhla 10 km.") 
@@ -46,7 +54,16 @@ def process(adresses : dist, bins : dist) -> tuple:
             max_dist = min_distance
             adress = [item['properties']['addr:street'], item['properties']['addr:housenumber']]
         min_distance = 0
-    return average/counter, adress, max_dist
+        # Zápis do souboru adresy_kontejnery.geojson
+    with open("adresy_kontejnery.geojson", "w", encoding = 'utf-8') as write_json:
+        json.dump(adresses, write_json, ensure_ascii = False, indent = 2)
+        # Výpočet mediánu
+    dist_array.sort()
+    if counter%2 == 1:
+        median = dist_array[counter//2]
+    else:
+        median = (dist_array[counter//2 - 1] + dist_array[counter//2])/2
+    return average/counter, median, adress, max_dist
 
 try:
     adresses = None         # slovník s adresami
@@ -68,13 +85,13 @@ try:
         if  not len(bins['features']) or not len(adresses['features']):
             print(">> nebyly načteny žádné adresy nebo žádné veřejné kontejnery.")
         else:
-            average, far_adress, max_dist = process(adresses, bins)
-            print(f"Prumerna vzdalenost ke kontejneru je {round(average, 0)} m.")
+            average, median, far_adress, max_dist = process(adresses, bins)
+            print(f"Prumerná minimální vzdalenost ke kontejneru je {round(average, 0)} m.")
+            print(f"Medián minimálních vzdáleností ke kontejnerům je {round(median, 0)} m.")
             print(f"Nejdále ke kontejneru je z adresy {far_adress[0]} {far_adress[1]} a to {round(max_dist, 0)} m.")
 except FileNotFoundError:
     print(">> Soubor s daným jménem neexistuje.")
 except PermissionError:
     print(">> Ke čtení souboru nemáte práva.")
-#except KeyError:
-    #print(">> Klíč nebyl ve slovníku nalezen.")
-
+except KeyError:
+    print(">> Klíč nebyl ve slovníku nalezen.")
